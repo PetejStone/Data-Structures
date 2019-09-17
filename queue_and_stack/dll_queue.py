acddc466c13d8bed9568571abf0e391e61f0a3c3
PetejStone <petejstoney@gmail.com>
2019-09-17 15:36:43 -0700

    passing stacks

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('../doubly_linked_list') 
 from doubly_linked_list import DoublyLinkedList
 
 class Queue:
@@ -9,7 +9,7 @@ class Queue:
     self.storage = DoublyLinkedList()
 
   def enqueue(self, value):
-    self.storage.add_to_tail(value)
+    self.storage.add_to_head(value)
     self.size += 1
  
   def dequeue(self):

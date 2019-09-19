904ea3cd81810d0aff7ac2f97f8988904c93457f
PetejStone <petejstoney@gmail.com>
2019-09-19 09:31:52 -0700

    have insert passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -11,7 +11,24 @@ class BinarySearchTree:
   #
 
   def insert(self, value):
-    pass
+    #current node is self
+    #check if self.value is > or < than new value -- left or right
+    if value < self.value:
+    #We go left or right, then check if node exists
+      if not self.left:
+    #if node does not exists, create it
+        self.left = BinarySearchTree(value)
+    #if node does exist, use recursion! Call insert on that specific node
+      else:
+        self.left.insert(value)
+    else:
+       #we go right
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
+
 
   def contains(self, target):
     pass

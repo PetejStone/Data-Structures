a971784561dbc02202d0fdafc0b18c1c384893cc
PetejStone <petejstoney@gmail.com>
2019-09-19 15:06:24 -0700

    started inorder

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,6 +3,29 @@ sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
+class Queue(object):
+  def __init__(self):
+    self.items = []
+
+  def enqueue(self, item):
+    self.items.insert(0,item)
+  
+  def dequeue(self):
+    if not self.is_empty():
+      return self.items.pop()
+
+  def is_empty(self):
+    return len(self.items) == 0
+
+  def peek(self):
+    if not self.is_empty():
+      return self.items[-1].value
+  def __len__(self):
+    return self.size()
+
+  def size(self):
+    return len(self.items)
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
@@ -52,7 +75,7 @@ class BinarySearchTree:
   def get_max(self):
     #max node == farthest to the right
     #base case
-    if no right, root is max
+    #if no right, root is max
     if not self.right:
       return self.value #root
     return self.right.get_max()
@@ -67,4 +90,73 @@ class BinarySearchTree:
 
 
   def for_each(self, cb):
-    pass
+    # Must use a callback function
+
+    cb(self.value)
+
+    #check for left or right then use recursion
+
+    if self.left:
+      self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
+
+# DAY 2 Project -----------------------
+
+  # Print all the values in order from low to high
+  # Hint:  Use a recursive, depth first traversal
+  def in_order_dft(node,self):
+  
+    # if self:
+    #   while self.left:
+    #     pass
+    #     #self.left.in_order_dft(self.left)
+    #   print(self.value)
+    #   return
+    #   while self.right:
+    #     pass
+    #     #self.right.in_order_dft(self.right)
+      
+    #   return
+  
+      
+  # Print the value of every node, starting with the given node,
+  # in an iterative breadth first traversal
+  def bft_print(self, start):
+    if start is None:
+      return '123'
+    queue = Queue()
+
+    queue.enqueue(start)
+    traversal = 0
+    while len(queue) > 0:
+      traversal += queue.peek() 
+      node = queue.dequeue()
+
+      if node.left:
+        queue.enqueue(node.left)
+      if node.right:
+        queue.enqueue(node.right)
+    return '123'
+      
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative depth first traversal
+  def dft_print(self, node):
+      if self.left:
+        self.left
+        self.right
+      else:
+        self.left
+        self.right
+
+  # STRETCH Goals -------------------------
+  # Note: Research may be required
+
+  # Print In-order recursive DFT
+  def pre_order_dft(self, node):
+      pass
+
+  # Print Post-order recursive DFT
+  def post_order_dft(self, node):
+      pass
